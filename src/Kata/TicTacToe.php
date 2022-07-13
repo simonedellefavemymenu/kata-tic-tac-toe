41275b1fd49030e2256989d67ca291d4a2615b44
MyMariettuz <mariospallazzi@mymenu.it>
2022-07-13 23:29:47 +0200

    chore: revert to begin

--- src/Kata/TicTacToe.php
+++ src/Kata/TicTacToe.php
@@ -4,20 +4,8 @@ namespace Kata;
 
 class TicTacToe
 {
-    public function playerTakeField(Player $player, Field $field): array
+    public function handle(): bool
     {
-        $moves = [];
-
-        if ($player->checkPlayer() === 'x') {
-            $moves['x']['abscissa'][] = $field->getAbscissa();
-            $moves['x']['ordered'][] = $field->getOrdered();
-        }
-
-        if ($player->checkPlayer() === 'y') {
-            $moves['y']['abscissa'][] = $field->getAbscissa();
-            $moves['y']['ordered'][] = $field->getOrdered();
-        }
-
-        return $moves;
+        return true;
     }
 }
