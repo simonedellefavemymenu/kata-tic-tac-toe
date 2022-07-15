644421935dcc7a1d0965c35492a256458b21806c
MyMariettuz <mariospallazzi@mymenu.it>
2022-07-15 09:32:01 +0200

    refactor: simplify playerTakeMove method

--- src/Kata/TicTacToe.php
+++ src/Kata/TicTacToe.php
@@ -6,26 +6,13 @@ class TicTacToe
 {
     public function playerTakeMove(Player $player, Field $field, Round $round, Coordinates $coordinates): Field
     {
-        if ($player->getIcon() === 'x') {
-            if ($round->getRound() === 1) {
-                $field->putIconIntoField('x', 0, 0);
-            }
-
-            if ($round->getRound() === 2) {
-                $field->putIconIntoField('x', 0, 2);
-            }
-
-            return $field;
-        }
-
         if ($round->getRound() === 1) {
-            $field->putIconIntoField('o', 0, 1);
+            $field->putIconIntoField($player->getIcon(), $coordinates->getRow(), $coordinates->getColumn());
         }
-
         if ($round->getRound() === 2) {
-            $field->putIconIntoField('o', 1, 0);
+            $field->putIconIntoField($player->getIcon(), $coordinates->getRow(), $coordinates->getColumn());
         }
-
+        
         return $field;
     }
 }
